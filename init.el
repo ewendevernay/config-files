e0d40f5c449cc995cbbbd31ff153f58566a179be
ewendevernay <58626131+ewendevernay@users.noreply.github.com>
2026-01-06 20:48:10 +0000

    Update init.el

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(dolist (p '(multiple-cursors yaml-mode toml-mode markdown-mode forth-mode))
+(dolist (p '(multiple-cursors yaml-mode toml-mode markdown-mode zenburn-theme))
   (when (not (package-installed-p p))
     (package-install p)))
 
@@ -13,6 +13,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(setq inhibit-startup-screen t)
 (setq make-backup-files nil)
 
 (ido-mode 1)
@@ -45,21 +46,80 @@
 (keymap-global-set "C-<home>" 'beginning-of-buffer)
 (keymap-global-set "C-<end>" 'end-of-buffer)
 (keymap-global-set "M-v" 'move-to-window-line-top-bottom)
+(keymap-global-set "<f5>" 'compile)
+
+(defun move-text-internal (arg)
+   (cond
+    ((and mark-active transient-mark-mode)
+     (if (> (point) (mark))
+            (exchange-point-and-mark))
+     (let ((column (current-column))
+              (text (delete-and-extract-region (point) (mark))))
+       (forward-line arg)
+       (move-to-column column t)
+       (set-mark (point))
+       (insert text)
+       (exchange-point-and-mark)
+       (setq deactivate-mark nil)))
+    (t
+     (beginning-of-line)
+     (when (or (> arg 0) (not (bobp)))
+       (forward-line)
+       (when (or (< arg 0) (not (eobp)))
+            (transpose-lines arg))
+       (forward-line -1)))))
+
+(defun move-text-down (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines down."
+   (interactive "*p")
+   (move-text-internal arg))
+
+(defun move-text-up (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines up."
+   (interactive "*p")
+   (move-text-internal (- arg)))
+
+(keymap-global-set "M-<up>" 'move-text-up)
+(keymap-global-set "M-<down>" 'move-text-down)
+
+(defun select-current-line ()
+  (interactive)
+  (if (and mark-active transient-mark-mode) ;; We are already selecting
+      (progn
+	(end-of-line)
+	(forward-char))
+    (beginning-of-line)
+    (set-mark (point))
+    (next-line)))
+
+(keymap-global-set "C-l" 'select-current-line)
+
+(require 'dired)
 (define-key dired-mode-map "\C-o" 'ido-find-file)
 
 (global-auto-revert-mode 1)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "M-S-<up>") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-S-<down>") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-d") 'mc/mark-next-like-this-word)
+(global-set-key (kbd "C-S-a") 'mc/mark-all-like-this)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wheatgrass))
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("972f792651d32b0506481b9e87b2fbc9b732ae9da2527562668c6e7d149fefda" default))
  '(package-selected-packages
-   '(forth-mode markdown-mode multiple-cursors toml-mode yaml-mode)))
+   '(zenburn-theme yaml-mode toml-mode multiple-cursors markdown-mode forth-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "Deja Vu Sans Mono" :slant normal :weight regular :height 110 :width normal)))))
